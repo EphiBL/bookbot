bd34a419b57ed85852dd6eba890747ab85fd149c
Ephi <rj_aguirre94@outlook.com>
2024-06-30 16:24:55 +0100

    final, formatting changes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+
 def main():
     book_path = "books/frankenstein.txt"
 
     book_text: str = read_book_file(book_path)
     word_count: int = count_words(book_text)
     character_frequencies: dict = count_characters(book_text)
-    frequencies: list = list_letters(character_frequencies)
+    frequencies: list = chars_dict_to_sorted_list(character_frequencies)
     frequencies.sort(reverse=True, key=sort_on)
 
     print(f"--- Begin Report of {book_path}  ---")
@@ -28,7 +29,7 @@ def read_book_file(path):
 def sort_on(dict):
     return dict["num"]
 
-def list_letters(char_frequencies: dict) -> list:
+def chars_dict_to_sorted_list(char_frequencies: dict) -> list:
     letters_list = []
 
     for char in char_frequencies:
@@ -43,10 +44,8 @@ def list_letters(char_frequencies: dict) -> list:
 
 def count_characters(text):
     letters = {}
-
     for word in text:
         lower_word = word.lower()
-
         for char in lower_word:
             if char not in letters:
                 letters[char] = 1
