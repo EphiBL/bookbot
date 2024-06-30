128fc05242c325017867cb218ab248dd9c163400
Ephi <rj_aguirre94@outlook.com>
2024-06-30 16:03:46 +0100

    character sorter finished

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 def main():
     book_path = "books/frankenstein.txt"
-    book_text = read_book_file(book_path)
-    word_count = count_words(book_text)
-    letter_count_dict = count_letters(book_text)
-    # print(book_text)
-    # print(word_count)
-    print(letter_count_dict)
+
+    book_text: str = read_book_file(book_path)
+    word_count: int = count_words(book_text)
+    character_frequencies: dict = count_characters(book_text)
+    frequencies: list = list_letters(character_frequencies)
+    frequencies.sort(reverse=True, key=sort_on)
+
+    print(f"--- Begin Report of {book_path}  ---")
+    print(f"There are {word_count} words in the book")
+    print(" ")
+
+    for letter in frequencies:
+        print(f"The '{letter['name']}' character occurs {letter['num']} times")
+
+    print("--- End Report ---")
 
 def count_words(text):
     words = text.split()
@@ -15,8 +24,24 @@ def read_book_file(path):
     with open(path) as f:
         return f.read()
 
-def count_letters(text):
+# Returns "num" k:v of dict
+def sort_on(dict):
+    return dict["num"]
 
+def list_letters(char_frequencies: dict) -> list:
+    letters_list = []
+
+    for char in char_frequencies:
+        if char.isalpha():
+            letter_dict = {
+                "name": char,
+                "num": char_frequencies[char]
+            }
+            letters_list.append(letter_dict)
+
+    return letters_list
+
+def count_characters(text):
     letters = {}
 
     for word in text:
@@ -29,7 +54,5 @@ def count_letters(text):
                 letters[char] += 1
 
     return letters
-
-
 
 main()
